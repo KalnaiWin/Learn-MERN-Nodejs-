b6887b8f09e11851e0ceeaba98e6f2dd5ab823fb
Phúc <nhbphuc2006@gmail.com>
2025-09-09 20:17:35 +0700

    Nodemon

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,22 @@ import express from "express";
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-  res.status(200).send("You have got 5 notes");
-
+  res.status(200).send("You have got 7 notes");
 });
+
+app.post("api/notes", (req, res) => {
+  res.status(201).json({ message: "Post created successfully" });
+});
+
+app.put("api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Post updated successfully" });
+});
+
+app.delete("api/notes/:id", (req, res) => {
+  res.status(200).json({ message: "Post deleted successfully" });
+});
+
+
 
 app.listen(5000, () => {
   console.log("Server started on PORT: 5000");
