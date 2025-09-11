0e6196a2a1e6a8658c7b7225b630558ec081ea7e
Phúc <nhbphuc2006@gmail.com>
2025-09-11 09:43:27 +0700

    Home Page in backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,19 +2,33 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-connectDB();
-
 // middleware
+app.use( // this is should be add first
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(express.json()); // --> allow to access req.body and take info by json
+app.use(rateLimiter);
+
+//  simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url} `);
+//   next();
+// });
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(port, () => {
-  console.log("Server started on PORT: ", port);
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log("Server started on PORT:", port);
+  });
 });
